658da740b8bb5671bd643043cfd1b3d8a459d1f6
Пилипенко Сергей Андреевич <SPilipenko@inno.local>
2025-11-06 01:38:00 +0300

    added math operations

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
+        System.out.print("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
 
-        System.out.println("Длина текста: " + text.length());
+        System.out.print("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
 
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность чисел: " + difference);
+
+        int composition = firstNumber * secondNumber;
+        System.out.println("Произведение чисел: " + composition);
+
+        if (secondNumber == 0) {
+            System.out.println("Частное чисел не посчитать, на 0 делить нельзя");
+        } else {
+            double quotient = (double) firstNumber / secondNumber;
+            System.out.println("Частное чисел: " + quotient);
+        }
     }
 }
